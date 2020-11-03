d0e52fca839819f8e35d5a4fb721af004deb13c6
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-11-03 17:35:34 +0100

    Styling homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './style.scss';
 import NavLink from './../NavLink';
-import Logo from './../Logo';
 
 export const Header = props => {
   return (
     <div className="header">
-      <Logo />
+      <p>Sound off</p>
       <NavLink link={props.link} to={props.to} />
     </div>
   );
--- src/views/WelcomeView.jsx
+++ src/views/WelcomeView.jsx
@@ -2,20 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Button from '../components/Button';
+// import Button from '../components/Button';
 import './WelcomeView.scss';
 
+import { ReactComponent as MarsImage } from './../assets/images/mars.svg';
+// import { ReactComponent as DownArrow } from './../assets/icons/down-arrow.svg';
 
 function WelcomeView() {
   return (
-    <div className="welcome">
-      <Header link="about" to="/about" />
-      <h1>Welcome to MARS</h1>
-      <img className="image-intro" src="/images/mars.png" alt="mars" />
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam atque aliquam soluta hic maiores non quos cupiditate commodi blanditiis consectetur reprehenderit eligendi similique sed voluptas excepturi, porro ut a nesciunt.</p>
-      <Link to="/game">
-        <Button name="Enter game" />
-      </Link>
+    <div className="welcome-view">
+      <Header link="About" to="/about" />
+      <div className="intro">
+        <h1 className="title-intro">MISSION MARS</h1>
+        <MarsImage className="image-intro" />
+      </div>
+      <div className="welcome-section">
+        <h2>Do you have what it takes to save life on Mars?</h2>
+        <h2>The future of humanity depends on you.</h2>
+        <Link to="/base">
+          <button className="button-base">Enter the base</button>
+        </Link>
+        <Link to="/game">
+          <button className="button-game">Enter the game</button>
+        </Link>
+        {/* <DownArrow /> */}
+        <h2>The year is 2030.</h2>
+        <p>
+          Humanity didn’t meet the challenge of climate change and we need a
+          planet B. As the international collaboration presented the final plans
+          today for the expansion of the Mars colony, it is becoming urgent to
+          solve the base multiple & important challenges.
+        </p>
+      </div>
+      <div className="welcome-info">
+        <h2>HOW TO PLAY</h2>
+      </div>
       <Footer />
     </div>
   );
