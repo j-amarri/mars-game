17d1a29b879d26b214ac1000608bdf8db965ed8a
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-11-13 14:54:36 +0100

    Icons beahviour

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import './style.scss'
+import React, { Component } from 'react';
+import './style.scss';
 
 class Button extends Component {
   render() {
-    const Icon = this.props.icon
+    const Icon = this.props.icon;
+    console.log(this.props.style);
     return (
       <div className="filters">
         <button value={this.props.value} onClick={this.props.onClick}>
           <img src={Icon} alt="icon" />
-          <span>{this.props.name}</span>
+          <span style={this.props.style}>{this.props.name}</span>
         </button>
       </div>
-    )
+    );
   }
 }
 
-export default Button
+export default Button;
--- src/views/GameView.jsx
+++ src/views/GameView.jsx
@@ -1,126 +1,149 @@
-import React from 'react'
-import Carousel from 'react-alice-carousel'
-import Fade from 'react-reveal/Fade' // Importing Zoom effect
-import 'react-alice-carousel/lib/scss/alice-carousel.scss'
-import './GameView.scss'
+import React from 'react';
+import Carousel from 'react-alice-carousel';
+import Fade from 'react-reveal/Fade'; // Importing Zoom effect
+import 'react-alice-carousel/lib/scss/alice-carousel.scss';
+import './GameView.scss';
 
-import cards from './../data/data-all.json'
+import cards from './../data/data-all.json';
 
-import Card from './../components/Card'
-import InfoPersona from '../components/InfoPersona'
-import InfoChallenge from '../components/InfoChallenge'
-import InfoTool from '../components/InfoTool'
-import Footer from './../components/Footer'
-import Button from './../components/Button'
+import Card from './../components/Card';
+import InfoPersona from '../components/InfoPersona';
+import InfoChallenge from '../components/InfoChallenge';
+import InfoTool from '../components/InfoTool';
+import Footer from './../components/Footer';
+import Button from './../components/Button';
 
-import PersonasInactive from './../assets/icons/personas-inactive.svg'
-import ToolsInactive from './../assets/icons/tools-inactive.svg'
-import ChallengesInactive from './../assets/icons/challenges-inactive.svg'
-import DeckInactive from './../assets/icons/deck-inactive.svg'
+import PersonasInactive from './../assets/icons/personas-inactive.svg';
+import PersonasActive from './../assets/icons/personas-active.svg';
+import ToolsInactive from './../assets/icons/tools-inactive.svg';
+import ToolsActive from './../assets/icons/tools-active.svg';
+import ChallengesInactive from './../assets/icons/challenges-inactive.svg';
+import ChallengesActive from './../assets/icons/challenges-active.svg';
+import DeckInactive from './../assets/icons/deck-inactive.svg';
 
 const responsive = {
   0: { items: 1 },
   568: { items: 2 },
-  1024: { items: 3 },
-}
+  1024: { items: 3 }
+};
 
 class GameView extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       loaded: false,
       category: 'persona',
       cardIndex: 0,
       allCards: cards,
       starred: [],
-      zoomAnimate: true,
-    }
+      zoomAnimate: true
+    };
   }
 
   componentDidMount() {
     this.setState({
-      loaded: true,
-    })
+      loaded: true
+    });
   }
 
-  filterByCategory = (event) => {
-    const category = event.currentTarget.value
+  filterByCategory = event => {
+    const category = event.currentTarget.value;
     this.setState({
-      category,
-    })
-  }
+      category
+    });
+  };
 
   get filteredCardsList() {
-    const category = this.state.category
+    const category = this.state.category;
     return category === 'starred'
       ? this.state.starred
-      : this.state.allCards.filter((card) => card.type === category)
+      : this.state.allCards.filter(card => card.type === category);
   }
 
   // to review
-  handleStarClick = (item) => {
+  handleStarClick = item => {
     // get main deck copy
-    const cardDeck = this.state.allCards.slice()
+    const cardDeck = this.state.allCards.slice();
     // get card from main deck
-    let card = cardDeck.find((elem) => {
-      return elem.title === item.title
-    })
+    let card = cardDeck.find(elem => {
+      return elem.title === item.title;
+    });
     // check if card is already in starred list
-    const index = this.state.starred.indexOf(card)
-    card.starred = index === -1 // if item is not on starred, make it starred
+    const index = this.state.starred.indexOf(card);
+    card.starred = index === -1; // if item is not on starred, make it starred
 
-    let items
+    let items;
     if (card.starred) {
-      items = [...this.state.starred.concat(card)]
+      items = [...this.state.starred.concat(card)];
     } else {
-      items = this.state.starred.slice() //copy starred array
-      items.splice(index, 1) // remove item from array
+      items = this.state.starred.slice(); //copy starred array
+      items.splice(index, 1); // remove item from array
     }
 
     this.setState({
       starred: items,
-      allCards: cardDeck,
-    })
-  }
+      allCards: cardDeck
+    });
+  };
 
-  onSlideChanged = (event) => {
+  onSlideChanged = event => {
     this.setState({
       zoomAnimate: false,
-      cardIndex: event.slide,
-    })
-    console.log('cardIndexUpdated', this.state.cardIndex)
+      cardIndex: event.slide
+    });
     this.setState({
-      zoomAnimate: true,
-    })
-  }
+      zoomAnimate: true
+    });
+  };
 
   render() {
-    const cardsToShow = this.filteredCardsList
+    const cardsToShow = this.filteredCardsList;
 
     return (
       <div className="game">
         <div className="categories-bar">
           <Button
-            icon={PersonasInactive}
+            icon={
+              this.state.category === 'persona'
+                ? PersonasActive
+                : PersonasInactive
+            }
             value={'persona'}
             name={'Persona'}
+            style={
+              this.state.category === 'persona'
+                ? { color: '#eb5d3a' }
+                : { color: 'white' }
+            }
             onClick={this.filterByCategory}
           />
 
           {/* <img src={ToolsInactive} alt="tools-icon" /> */}
           <Button
-            icon={ToolsInactive}
+            icon={this.state.category === 'tool' ? ToolsActive : ToolsInactive}
             value={'tool'}
             name={'Tools'}
-            //style={{ border: '1px solid #e6004c', color: '#e6004c' }}
+            style={
+              this.state.category === 'tool'
+                ? { color: '#ffa55e' }
+                : { color: 'white' }
+            }
             onClick={this.filterByCategory}
           />
 
           <Button
-            icon={ChallengesInactive}
+            icon={
+              this.state.category === 'challenge'
+                ? ChallengesActive
+                : ChallengesInactive
+            }
             value={'challenge'}
             name={'Challenges'}
-            //style={{ border: '1px solid #1e8fd5', color: '#1e8fd5' }}
+            style={
+              this.state.category === 'challenge'
+                ? { color: '#b4bff3' }
+                : { color: 'white' }
+            }
             onClick={this.filterByCategory}
           />
 
@@ -140,12 +163,12 @@ class GameView extends React.Component {
           disableDotsControls={true}
           onSlideChanged={this.onSlideChanged}
         >
-          {cardsToShow.map((item) => {
+          {cardsToShow.map(item => {
             return (
               <div key={item.title}>
                 <Card {...item} starClick={this.handleStarClick} />
               </div>
-            )
+            );
           })}
         </Carousel>
 
@@ -156,8 +179,8 @@ class GameView extends React.Component {
         </Fade>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default GameView
+export default GameView;
